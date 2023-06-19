5a2a071bf1131a4e2b0a8c1659bc301bb00d27f6
Bitalizer <hungryjesus@gmail.com>
2023-06-19 16:47:12 +0300

    Implemented VehicleDataSeeder

---
+++ src/main/java/com/example/automarket/domain/json/BrandModelJson.java
@@ -0,0 +1,12 @@
+package com.example.automarket.domain.json;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class BrandModelJson {
+
+    private String brand;
+    private List<String> models;
+}
