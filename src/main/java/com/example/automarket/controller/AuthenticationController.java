bee5ee684cef1c6cb15612a0b639369be4d829a0
Bitalizer <hungryjesus@gmail.com>
2023-06-19 16:37:05 +0300

    AuthenticationController typo fix

--- src/main/java/com/example/automarket/controller/AuthenticationController.java
+++ src/main/java/com/example/automarket/controller/AuthenticationController.java
@@ -11,6 +11,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,18 +21,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 @Slf4j
+@Validated
 public class AuthenticationController {
 
     private final AuthenticationService authenticationService;
 
     @PostMapping("/register")
     public ResponseEntity<JwtAuthenticationResponse> register(@Valid @RequestBody RegistrationRequest request) {
-        log.info("test register");
         return ResponseEntity.ok(authenticationService.register(request));
     }
 
     @PostMapping("/authenticate")
-    public ResponseEntity<JwtAuthenticationResponse> autenticate(@RequestBody AuthenticationRequest request) {
+    public ResponseEntity<JwtAuthenticationResponse> authenticate(@Valid @RequestBody AuthenticationRequest request) {
         return ResponseEntity.ok(authenticationService.authenticate(request));
     }
 
