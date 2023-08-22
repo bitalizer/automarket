c22b47eeb6ceef034d7a4eb4ad0fd452feb68edd
Bitalizer <hungryjesus@gmail.com>
2023-08-23 00:18:34 +0300

    Enhance Refresh Token Uniqueness
    Modify the token generation logic in the authentication process to enhance the uniqueness of refresh tokens. Instead of relying solely on System.currentTimeMillis(), the buildToken method now incorporates a UUID.randomUUID() to generate a globally unique identifier for each refresh token.
    
    This change ensures that refresh tokens are distinctly unique, even in cases of rapid token generation, and enhances the security and reliability of the token-based authentication system.

--- src/main/java/com/example/automarket/service/impl/JwtServiceImpl.java
+++ src/main/java/com/example/automarket/service/impl/JwtServiceImpl.java
@@ -14,6 +14,7 @@ import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 import java.util.function.Function;
 
 @Service
@@ -57,9 +58,11 @@ public class JwtServiceImpl implements JwtService {
             UserDetails userDetails,
             long expiration
     ) {
+        String tokenId = UUID.randomUUID().toString();
         return Jwts
                 .builder()
                 .setClaims(extraClaims)
+                .setId(tokenId)
                 .setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + expiration))
