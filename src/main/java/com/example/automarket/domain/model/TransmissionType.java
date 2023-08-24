ded80fd1e19861eca0ba6b9177e7d2ffdb4c508c
Bitalizer <hungryjesus@gmail.com>
2023-08-24 21:06:41 +0300

    Implement TransmissionType

---
+++ src/main/java/com/example/automarket/domain/model/TransmissionType.java
@@ -0,0 +1,11 @@
+package com.example.automarket.domain.model;
+
+public enum TransmissionType {
+    MANUAL,
+    AUTOMATIC,
+    CVT, // Continuously Variable Transmission
+    DCT, // Dual-Clutch Transmission
+    AMT, // Automated Manual Transmission
+    SEMI_AUTOMATIC, // Semi-Automatic Transmission
+    OTHER
+}
