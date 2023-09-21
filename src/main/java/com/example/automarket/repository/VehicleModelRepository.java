1dba48e4eadb25532f2e292a0881a739ca6c529d
Bitalizer <hungryjesus@gmail.com>
2023-09-21 16:45:23 +0300

    refactor: extend JpaSpecificationExecutor in VehicleModelRepository instead of hardcoded method

--- src/main/java/com/example/automarket/repository/VehicleModelRepository.java
+++ src/main/java/com/example/automarket/repository/VehicleModelRepository.java
@@ -1,15 +1,11 @@
 package com.example.automarket.repository;
 
 import com.example.automarket.domain.model.listing.VehicleModel;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
-public interface VehicleModelRepository extends JpaRepository<VehicleModel, Long> {
-
-    List<VehicleModel> findAll(Specification<VehicleModel> spec);
+public interface VehicleModelRepository extends JpaRepository<VehicleModel, Long>, JpaSpecificationExecutor<VehicleModel> {
 
 }
